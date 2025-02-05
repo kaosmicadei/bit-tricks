76c4194bc2e03b03f262bbab82e54d8598f9aabe
Kaonan Micadei <k.micadei@gmail.com>
2025-02-05 13:27:24 +0100

    clean comments

--- nrank_multiplication/main.jl
+++ nrank_multiplication/main.jl
@@ -2,50 +2,48 @@ using Test
 
 """Swaps the *i*-th bit from a number `n` with its least significant bit. 
 Adapted from:
-        http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
+    http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
 """
 function bit_swap(bit_index::Int64, number::Int64)::Int64
-    # Checks if the target bits are the different.
-    x = (number ⊻ (number >> bit_index)) & 1
+  # Checks if the target bits are the different.
+  x = (number ⊻ (number >> bit_index)) & 1
 
-    # Flip the target and the first bit.
-    number ⊻ ((x << bit_index) | x)
+  # Flip the target and the first bit.
+  number ⊻ ((x << bit_index) | x)
 end
 
 function multiply(matrix::Matrix{Float32}, vector::Vector{Float32})::Vector{Float32}
-    vector_length = length(vector)
-    num_qubits = log2(vector_length) |> Int64
-    acc = vector |> copy |> v -> reshape(v, 2, :)
-    for i in 0:(num_qubits-1)
-        acc .= matrix * reshape(acc[bit_swap.(i, 0:(vector_length-1)).+1], 2, :)
-    end
-    transpose(acc) |> vec
+  vector_length = length(vector)
+  num_qubits = log2(vector_length) |> Int64
+  acc = vector |> copy |> v -> reshape(v, 2, :)
+  for i in 0:(num_qubits-1)
+    acc .= matrix * reshape(acc[bit_swap.(i, 0:(vector_length-1)).+1], 2, :)
+  end
+  transpose(acc) |> vec
 end
 
 
 # <<<TESTS>>>
 function main()
-    # Number of qubits.
-    N = isempty(ARGS) ? 3 : parse(Int, ARGS[1])
+  # Number of qubits.
+  N = isempty(ARGS) ? 3 : parse(Int, ARGS[1])
 
-    # Simulate random input.
-    x = rand(Float32, 2^N)
+  # Simulate random input.
+  x = rand(Float32, 2^N)
 
-    # Confusion matrix: [a₀₀ a₀₁ ; a₁₀ a₁₁].
-    m::Matrix{Float32} = inv([0.9 0.3; 0.1 0.7])
+  # Confusion matrix: [a₀₀ a₀₁ ; a₁₀ a₁₁].
+  m::Matrix{Float32} = inv([0.9 0.3; 0.1 0.7])
 
-    print("reshape: ")
-    @time res1 = multiply(m, x)
-    # On an Apple M1 Pro it took around 35 seconds for N=28.
+  print("reshape: ")
+  @time res1 = multiply(m, x)
 
-    # On an Apple M1 Pro, the limit for `kron` was N=15.
-    if N <= 15
-        print("kron: ")
-        @time res2::Vector{Float32} = kron(fill(m, N)...) * x
-        @test res1 ≈ res2
-    end
+  print("kron: ")
+  @time res2::Vector{Float32} = kron(fill(m, N)...) * x
+  @test res1 ≈ res2
 end
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    main()
-end
+main()
+
+# Example for N=15 on an Apple M1.
+# reshape:   0.002849 seconds (157 allocations: 7.754 MiB)
+# kron:   2.011607 seconds (37.58 k allocations: 5.335 GiB, 0.43% gc time, 1.92% compilation time) 
