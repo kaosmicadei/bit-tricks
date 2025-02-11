14bc00d7d2971448a50d4d9ad2fdea9e1c7f639c
Kaonan Micadei <k.micadei@gmail.com>
2025-02-11 08:37:22 +0100

    rename `bit_swap` function

--- nrank_multiplication/NRankMul/src/NRankMul.jl
+++ nrank_multiplication/NRankMul/src/NRankMul.jl
@@ -1,12 +1,12 @@
 module NRankMul
 
-export bit_swap, multiply
+export bitswap0, multiply
 
 """Swaps the *i*-th bit from a number `n` with its least significant bit. 
 Adapted from:
     http://graphics.stanford.edu/~seander/bithacks.html#SwappingBitsXOR
 """
-function bit_swap(bit_index::Int64, number::Int64)::Int64
+function bitswap0(bit_index::Int64, number::Int64)::Int64
   # Checks if the target bits are the different.
   x = (number ⊻ (number >> bit_index)) & 1
 
@@ -19,7 +19,7 @@ function multiply(matrix::Matrix{Float32}, vector::Vector{Float32})::Vector{Floa
   num_qubits = log2(vector_length) |> Int64
   acc = vector |> copy |> v -> reshape(v, 2, :)
   for i in 0:(num_qubits-1)
-    acc .= matrix * reshape(acc[bit_swap.(i, 0:(vector_length-1)).+1], 2, :)
+    acc .= matrix * reshape(acc[bitswap0.(i, 0:(vector_length-1)).+1], 2, :)
   end
   transpose(acc) |> vec
 end
